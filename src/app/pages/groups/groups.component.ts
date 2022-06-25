0c04a04f9328b5bd9c1dc0d57f7e2f4a94753ed5
Josué Casasola <jcasasola@musicbattles.mx>
2022-06-25 10:15:06 -0500

    gitflow-feature-stash: groups

--- src/app/pages/groups/groups.component.ts
+++ src/app/pages/groups/groups.component.ts
@@ -1,4 +1,7 @@
+import { CdkDragDrop, CdkDragEnter, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
+import { Subject, takeUntil } from 'rxjs';
+import { GroupsService } from './services/groups.service';
 
 @Component({
   selector: 'app-groups',
@@ -6,10 +9,106 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./groups.component.scss']
 })
 export class GroupsComponent implements OnInit {
+  groups: any;
+  employeesListDrop = new Array;
+  employeesSelectedList = new Array;
 
-  constructor() { }
+  readonly UnSubscribe: Subject<void> = new Subject<void>();
+
+
+  constructor(
+    private groupsService: GroupsService
+  ) { }
 
   ngOnInit(): void {
+    this.getData();
   }
+
+  getData() {
+    const { groupsService, UnSubscribe } = this;
+    groupsService
+      .getGroups()
+      .pipe(takeUntil(UnSubscribe))
+      .subscribe((resp: any) => {
+        const { data: { groups } } = resp;
+        this.groups = groups
+      });
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+      this.searchEmployees(event.container.data, event.currentIndex)
+    }
+  }
+
+
+  searchEmployees(array: any, position: any) {
+    const { groupsService, UnSubscribe } = this;
+    const group = array[position];
+    const { id } = group;
+    const that = this;
+    groupsService
+      .getListEmployeesByIdGroup(id)
+      .pipe(takeUntil(UnSubscribe))
+      .subscribe({
+        next(resp: any) {
+          const { data: { employees } } = resp;
+          const employeesMo = employees.map(
+            (x: any) => {
+              return ({ ...x, active: true })
+            }
+          );
+          that.employeesListDrop[position].employees = employeesMo;
+        },
+        error(err) {
+          alert('Ups algo falló')
+        }
+      });
+  }
+
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    const result = this.groups.filter((x: any) => x.name.includes(filterValue))
+    this.groups = result;
+  }
+
+
+  changeStatus(check: boolean, indexGroup: any, indexEmployee: any) {
+    this.employeesListDrop[indexGroup].employees[indexEmployee].active = check;
+    console.log(this.employeesListDrop);
+  }
+
+  showEmployeesSelected() {
+    this.employeesSelectedList = [];
+    let employeesSelected = new Array;
+    this.employeesListDrop.map(
+      (x: any) => {
+        if (x.employees) {
+          x.employees.map(
+            (y: any) => {
+
+              if (y.active) {
+                employeesSelected.push({ ...y });
+              }
+            }
+          );
+        }
+      }
+    );
+    this.employeesSelectedList = employeesSelected;
+    console.log('Empleados Seleccionados');    
+    console.log(this.employeesSelectedList);
+    
+  }
+
 
 }
---
+++ src/app/pages/groups/services/groups.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GroupsService {
+
+  readonly url = environment.endpoint;
+
+  constructor(private http: HttpClient) { }
+
+  getGroups() {
+    return this.http.get(`${environment.url}${this.url.groups.get.groups}`);
+  }
+
+  getListEmployeesByIdGroup( id: string) {
+    return this.http.get(`${environment.url}${this.url.groups.get.employeesByIdGroup}${id}`);
+  }
+}
