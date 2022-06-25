ed4677802751564b9ae838f8e3a1de1384ddab9d
Josué Casasola <jcasasola@musicbattles.mx>
2022-06-25 11:21:50 -0500

    core(environment): update environment vars

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
+import { ENDPOINTS } from "./endpoints";
+
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://6edeayi7ch.execute-api.us-east-1.amazonaws.com/',
+  endpoint: ENDPOINTS,
 };
